0a1dbcaea38da0fe5cebb6543331466be000f781
batrakov <phoenixed.451@gmail.com>
2017-12-01 12:16:39 +0300

    upload

--- app/src/main/java/com/example/batrakov/broadcastreceivertask/MainActivity.java
+++ app/src/main/java/com/example/batrakov/broadcastreceivertask/MainActivity.java
@@ -1,36 +1,25 @@
 package com.example.batrakov.broadcastreceivertask;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
 import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import static android.content.ContentValues.TAG;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final long JOB_DELAY = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent sensorsListenerServiceIntent = new Intent(this, SensorsListenerService.class);
-        startService(sensorsListenerServiceIntent);
-        bindService(sensorsListenerServiceIntent, mServiceConnection, 0);
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        if (jobScheduler != null) {
+            ComponentName componentName = new ComponentName(getBaseContext(), NotificationJobService.class);
+            JobInfo jobInfo = new JobInfo.Builder(0, componentName)
+                    .setMinimumLatency(JOB_DELAY)
+                    .setPersisted(true).build();
+            jobScheduler.schedule(jobInfo);
+        }
     }
-
-    private ServiceConnection mServiceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            Log.i(TAG, "onServiceConnected: ");
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            Log.i(TAG, "onServiceDisconnected: ");
-        }
-    };
 }
